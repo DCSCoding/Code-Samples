c8eb933b5700ca8c4532e1a4ba883755989850f4
Darren <97865809+DCSCoding@users.noreply.github.com>
2022-01-30 18:50:31 +0900

    Update Main.py

--- Smallest Greater Sparse Number/Main.py
+++ Smallest Greater Sparse Number/Main.py
@@ -11,7 +11,7 @@ i = len(num)-1
 
 # The algorithm works as follows: starts at the end of the number, and checks if that digit and the digit in the column 
 # to the left are both equal to 1. If they are, it checks if the digit two columns to the left is a 0.
-# If it is, it sets num[i] and num[i-1] to 0, and num[i-2] to 1. It then decrement i and continues.
+# If it is, it sets num[i] and num[i-1] to 0, and num[i-2] to 1. It then decrements i and continues.
 # Otherwise, it sests only num[i] to 0, decrements i and continues.
 # If it reaches the end of the number and the number still is not sparse, (there is a 1 in the two largest order digits)
 # it sets i (the second largest order digit) to 0, and adds a 0 to the end of the array. 
